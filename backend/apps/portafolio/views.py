55f0e2d1896df0bff11c2a51870930824c475b4c
JuanSPuentes <puentesjs903@gmail.com>
2022-12-28 19:41:20 -0500

    Cambio de los permisos en el backend

--- backend/apps/portafolio/views.py
+++ backend/apps/portafolio/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status, permissions
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 #Modelos
 from .models import Portafolio, Tag
 from .serializers import PortafolioSerializer
@@ -10,7 +10,7 @@ from .serializers import PortafolioSerializer
 from .pagination import StandardResultsSetPagination
 
 class ProjectListView(ModelViewSet):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Portafolio.postobjects.all().order_by('-published')
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PortafolioSerializer
